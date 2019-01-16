21ae3bea68610add15e1d2ce48b0981dbcd87348
NagsTheProgrammer <34460480+NagsTheProgrammer@users.noreply.github.com>
2019-01-16 13:48:01 -0700

    end of class push

--- src/Main.java
+++ src/Main.java
@@ -1,2 +1,15 @@
+import java.io.*;
+import java.util.*;
+
 public class Main {
+
+    public static void main(String[] args){
+
+        System.out.println("Please choose which algorithm you'd like to run:\n\n1 (Bubble Sort)\n2 (Insertion Sort)\nOther");
+        FileReader in = new FileReader
+
+
+    }
+
+
 }
